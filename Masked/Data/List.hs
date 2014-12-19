e00d2577c51452985da7a2411c6a0530f25568ac
joe9 <joe9mail@gmail.com>
2014-12-19 11:40:46 -0600

    do not hide filter as there is no alternative

--- Masked/Data/List.hs
+++ Masked/Data/List.hs
@@ -5,9 +5,9 @@ module Masked.Data.List
 
 -- from http://www.stephendiehl.com/what/#prelude
 import           Data.List as Exports hiding (all, and, any, concat,
-                                       concatMap, elem, filter, find,
-                                       foldl, foldl', foldl1, foldr,
-                                       foldr1, mapAccumL, mapAccumR,
-                                       maximum, maximumBy, minimum,
-                                       minimumBy, notElem, or,
-                                       product, sum, (!!))
+                                       concatMap, elem, find, foldl,
+                                       foldl', foldl1, foldr, foldr1,
+                                       mapAccumL, mapAccumR, maximum,
+                                       maximumBy, minimum, minimumBy,
+                                       notElem, or, product, sum,
+                                       (!!))
